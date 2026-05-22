68258cb01048d060ef3410e7ca23fe7a03bc1b5c
Jasper Jackson <jasper.jackson1@yjb.gov.uk>
2026-05-22 10:21:57 +0100

    Add healthcheck package and endpoints from Justice Digital

--- apps/frontend/server/config.ts
+++ apps/frontend/server/config.ts
@@ -15,7 +15,7 @@ const requiredInProduction = { requireInProduction: true }
 
 export default {
   buildNumber: get('BUILD_NUMBER', '1_0_0', requiredInProduction),
-  productId: get('PRODUCT_ID', 'UNASSIGNED', requiredInProduction),
+  productId: get('PRODUCT_ID', 'YJB_UNASSIGNED', requiredInProduction),
   gitRef: get('GIT_REF', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
   branchName: get('GIT_BRANCH', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
   production,
--- apps/frontend/server/middleware/setUpHealthChecks.ts
+++ apps/frontend/server/middleware/setUpHealthChecks.ts
@@ -1,12 +1,17 @@
 import express, { Router } from 'express'
 
+import { monitoringMiddleware } from '@ministryofjustice/hmpps-monitoring'
 import type { ApplicationInfo } from '../applicationInfo'
-import config from '../config'
 
 export default function setUpHealthChecks(applicationInfo: ApplicationInfo): Router {
   const router = express.Router()
 
-  //perform health checks here
+  const middleware = monitoringMiddleware({
+    applicationInfo,
+  })
+
+  router.get('/info', middleware.info)
+  router.get('/ping', middleware.ping)
 
   return router
 }
