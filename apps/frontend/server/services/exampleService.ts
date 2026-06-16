5f424255096fb75ec25942f36d5c923b0f551d2b
Jasper <54336703+JazJax@users.noreply.github.com>
2026-06-16 09:41:50 +0100

    Feat: Update pipelines to deploy frontend app (#9)
    - Updating pipelines to deploy the 'real' UI, not the fake nginx
    placeholder
    - At this stage we're passing an API_URL environment variable of
    `https://localhost:3000` - which is the UI app
    - This is necessary to fake healthiness of the dependency and enable
    kubernetes to greenlight replacement of an existing deployed app
    version.
      - We still need to deploy the actual API
    
    ## Before:
    <img width="1008" height="697" alt="Screenshot 2026-06-15 at 14 42 34"
    src="https://github.com/user-attachments/assets/3bd59709-ee2c-4518-988b-834aee9cb30c"
    />
    Deploying a static nginx placeholder
    
    ## After:
    <img width="679" height="344" alt="Screenshot 2026-06-16 at 09 17 48"
    src="https://github.com/user-attachments/assets/416e1340-9ccc-4c21-90d6-f696c7a23fd4"
    />

--- apps/frontend/server/services/exampleService.ts
+++ apps/frontend/server/services/exampleService.ts
@@ -1,6 +1,23 @@
 export default class ExampleService {
   // just a test function returning current time
-  getCurrentTime() {
-    return new Date().toISOString()
+  getCurrentTime(): string {
+    const now = new Date()
+
+    const time = now.toLocaleString('en-GB', {
+      timeZone: 'Europe/London',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+    })
+
+    const date = now.toLocaleString('en-GB', {
+      timeZone: 'Europe/London',
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+
+    return `${time} on ${date}`
   }
 }
