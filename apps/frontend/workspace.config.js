29567b4dcffd73f2553f27e0be14bc8224e2c6f4
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-06-17 15:49:56 +0100

    remove spaces and semicolumn to make lint happy

--- apps/frontend/workspace.config.js
+++ apps/frontend/workspace.config.js
@@ -1,6 +1,6 @@
 const path = require('node:path')
 
-const WORKSPACE_ROOT = __dirname;   // always apps/frontend
+const WORKSPACE_ROOT = __dirname // always apps/frontend
 const REPOSITORY_ROOT = path.resolve(WORKSPACE_ROOT, '../..')
 const NODE_MODULES = path.resolve(REPOSITORY_ROOT, './node_modules')
 
