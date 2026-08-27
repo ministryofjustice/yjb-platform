94aa02e8d13dacf848984b1f0034f49d374a7851
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-08-27 15:12:49 +0100

    default test is dedundant replace with real test and implementaion

--- apps/api/routes/sentenceCalculator.ts
+++ apps/api/routes/sentenceCalculator.ts
@@ -1,10 +1,19 @@
 import { Router, Request, Response } from 'express'
 import SentenceCalculator from '../services/sentenceCalculator/SentenceCalculator'
-import { Sentence, Term, Reason } from '../services/sentenceCalculator/types'
-
+import { Sentence } from '../services/sentenceCalculator/types'
 
 export function calculateSentence(req: Request, res: Response): void {
-  res.status(200).json()
+  const sentence = req.body as Sentence
+
+  // construct a sentence object from deserialized input onbect
+  // TODO: extract this in parser class which also does simple validation
+  sentence.term = sentence.term.map(term => ({ ...term, from: new Date(term.from) }))
+
+  // TODO: introduce a controller which determines if adjustmnets should be
+  // made based on remand / tag bail or not
+  const calculator = new SentenceCalculator(sentence)
+
+  res.status(200).json(calculator.getCalculation())
 }
 
 export default function sentenceCalculatorRoutes(): Router {
