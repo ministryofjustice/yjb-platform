630fc22a657aa4274d5fe07f1ad18c1cf5449a76
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-08-27 15:12:49 +0100

    default test is dedundant replace with real test and implementaion

--- apps/api/routes/sentenceCalculator.test.ts
+++ apps/api/routes/sentenceCalculator.test.ts
@@ -3,10 +3,30 @@ import express from 'express'
 import sentenceCalculatorRoutes from './sentenceCalculator'
 
 const app = express()
+app.use(express.json())
 app.use(sentenceCalculatorRoutes())
 
-describe('GET /calculations just returns 200', () => {
-    it('should return 200', () => {
-        return request(app).post('/calculations').expect(200)
-    })
+describe('POST /calculations', () => {
+  it('should return valid calculations for a valid input of sentence with no remand', () => {
+    const input = {
+      term: [
+        {
+          from: '2026-06-29',
+          durationMonths: 11,
+          offenderName: 'Test Offender',
+          remand: 0,
+        },
+      ],
+    }
+
+    const output = {
+      totalDaysInTerm: 334,
+      sledDate: '2027-05-28T00:00:00.000Z',
+      totalDaysMTD: 167,
+      mtdDate: '2026-12-12T00:00:00.000Z',
+      adjustmentRecords: [],
+    }
+
+    return request(app).post('/calculations').send(input).expect(200, output)
+  })
 })
