20df0ca55a8dd5d1abeeeb189cb446c6f5d80daa
Jasper Jackson <jasper.jackson1@yjb.gov.uk>
2026-08-27 09:17:13 +0100

    Add sentence type and date picker to the new calculation page, and a link to the page from start

--- apps/frontend/server/views/pages/new-calculation.test.ts
+++ apps/frontend/server/views/pages/new-calculation.test.ts
@@ -10,8 +10,52 @@ describe('New calculation page', () => {
     expect(cheerioPage('title').text()).toBe('Youth Justice Platform - New calculation')
   })
 
-  it('renders the page heading', () => {
+  it('renders the page heading and subheading', () => {
     const cheerioPage = renderWithCheerio()
     expect(cheerioPage('h1').text()).toBe('New calculation')
+    expect(cheerioPage('h2').text()).toBe('Offence and sentence details')
+  })
+
+  it('renders the calculate button', () => {
+    const cheerioPage = renderWithCheerio()
+    const continueButton = cheerioPage('#calculate-button')
+    expect(continueButton.text()).toContain('Calculate')
+    expect(continueButton.prop('href')).toBe('/calculation-breakdown')
+  })
+
+  it('renders the expected text', () => {
+    const cheerioPage = renderWithCheerio()
+    expect(cheerioPage('#instruction-text').text()).toBe('Enter the calculation details below')
+  })
+
+  it('renders the sentence type dropdown', () => {
+    const cheerioPage = renderWithCheerio()
+
+    const typeInput = cheerioPage('#sentence-type')
+    expect(typeInput.length).toBe(1)
+
+    const dropdownOptions = typeInput
+      .find('option')
+      .toArray()
+      .map(el => cheerioPage(el))
+
+    // check default option is blank
+    expect(dropdownOptions[0].attr('disabled')).toBeDefined()
+    expect(dropdownOptions[0].attr('selected')).toBeDefined()
+    expect(dropdownOptions[0].text().trim()).toBe('')
+
+    // check options are correct
+    expect(dropdownOptions[1].text().trim()).toBe('Some sentence that is applicable to DTO')
+    expect(dropdownOptions[1].attr('value')).toBe('some-sentence-code')
+  })
+
+  it('renders the sentence date input', () => {
+    const cheerioPage = renderWithCheerio()
+
+    const dateInput = cheerioPage('#sentence-date')
+    expect(dateInput.length).toBe(1)
+
+    const dateHeading = cheerioPage('legend')
+    expect(dateHeading.text()).toContain('Sentence date')
   })
 })
