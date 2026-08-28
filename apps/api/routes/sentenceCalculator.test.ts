1252cd26f8db4f2c3f05b4fb2beabd402b50c7a1
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-08-28 14:48:27 +0100

    effective LTD added to sentence calc

--- apps/api/routes/sentenceCalculator.test.ts
+++ apps/api/routes/sentenceCalculator.test.ts
@@ -24,6 +24,8 @@ describe('POST /calculations', () => {
       effectiveSledDate: '2027-05-28T00:00:00.000Z',
       totalDaysMTD: 167,
       effectiveMTDDate: '2026-12-12T00:00:00.000Z',
+      effectiveLTD: '2026-08-28T00:00:00.000Z',
+      effectiveETD: '2026-08-28T00:00:00.000Z',
       adjustmentRecords: [],
     }
 
