e7ab9c27d49743650c458949af5294cc12d27a3f
Jasper Jackson <jasper.jackson1@yjb.gov.uk>
2026-09-02 09:36:16 +0100

    Move inputs and button into form element

--- apps/frontend/server/views/pages/new-calculation.test.ts
+++ apps/frontend/server/views/pages/new-calculation.test.ts
@@ -5,57 +5,71 @@ const env = createNunjucksTestSetup()
 const renderWithCheerio = () => cheerio.load(env.render('pages/new-calculation.njk'))
 
 describe('New calculation page', () => {
-  it('renders the tab title', () => {
-    const cheerioPage = renderWithCheerio()
-    expect(cheerioPage('title').text()).toBe('Youth Justice Platform - New calculation')
+
+  describe('content', () => {
+    it('renders the tab title', () => {
+      const cheerioPage = renderWithCheerio()
+      expect(cheerioPage('title').text()).toBe('Youth Justice Platform - New calculation')
+    })
+
+    it('renders the page heading and subheading', () => {
+      const cheerioPage = renderWithCheerio()
+      expect(cheerioPage('h1').text()).toBe('New calculation')
+      expect(cheerioPage('h2').text()).toBe('Offence and sentence details')
+    })
+
+    it('renders the expected text', () => {
+      const cheerioPage = renderWithCheerio()
+      expect(cheerioPage('#instruction-text').text()).toBe('Enter the calculation details below')
+    })
   })
 
-  it('renders the page heading and subheading', () => {
-    const cheerioPage = renderWithCheerio()
-    expect(cheerioPage('h1').text()).toBe('New calculation')
-    expect(cheerioPage('h2').text()).toBe('Offence and sentence details')
+  describe('logic', () => {
+    it('contains a calculate button', () => {
+      const cheerioPage = renderWithCheerio()
+      const continueButton = cheerioPage('#calculate-button')
+      expect(continueButton.text()).toContain('Calculate')
+    })
+
+    it('has a submission form for /calculate', () => {
+      const cheerioPage = renderWithCheerio()
+      const inputForm = cheerioPage('#calculation-form')
+      expect(inputForm.prop('method')).toBe('post')
+      expect(inputForm.prop('action')).toBe('/calculate')
+    })
   })
 
-  it('renders the calculate button', () => {
-    const cheerioPage = renderWithCheerio()
-    const continueButton = cheerioPage('#calculate-button')
-    expect(continueButton.text()).toContain('Calculate')
-    expect(continueButton.prop('href')).toBe('/calculation-breakdown')
+  describe('inputs', () => {
+    it('includes the sentence type dropdown', () => {
+      const cheerioPage = renderWithCheerio()
+
+      const typeInput = cheerioPage('#sentence-type')
+      expect(typeInput.length).toBe(1)
+
+      const dropdownOptions = typeInput
+        .find('option')
+        .toArray()
+        .map(el => cheerioPage(el))
+
+      // check default option is blank
+      expect(dropdownOptions[0].attr('disabled')).toBeDefined()
+      expect(dropdownOptions[0].attr('selected')).toBeDefined()
+      expect(dropdownOptions[0].text().trim()).toBe('')
+
+      // check options are correct
+      expect(dropdownOptions[1].text().trim()).toBe('Some sentence that is applicable to DTO')
+      expect(dropdownOptions[1].attr('value')).toBe('some-sentence-code')
+    })
+
+    it('includes the sentence date input', () => {
+      const cheerioPage = renderWithCheerio()
+
+      const dateInput = cheerioPage('#sentence-date')
+      expect(dateInput.length).toBe(1)
+
+      const dateHeading = cheerioPage('legend')
+      expect(dateHeading.text()).toContain('Sentence date')
+    })
   })
 
-  it('renders the expected text', () => {
-    const cheerioPage = renderWithCheerio()
-    expect(cheerioPage('#instruction-text').text()).toBe('Enter the calculation details below')
-  })
-
-  it('renders the sentence type dropdown', () => {
-    const cheerioPage = renderWithCheerio()
-
-    const typeInput = cheerioPage('#sentence-type')
-    expect(typeInput.length).toBe(1)
-
-    const dropdownOptions = typeInput
-      .find('option')
-      .toArray()
-      .map(el => cheerioPage(el))
-
-    // check default option is blank
-    expect(dropdownOptions[0].attr('disabled')).toBeDefined()
-    expect(dropdownOptions[0].attr('selected')).toBeDefined()
-    expect(dropdownOptions[0].text().trim()).toBe('')
-
-    // check options are correct
-    expect(dropdownOptions[1].text().trim()).toBe('Some sentence that is applicable to DTO')
-    expect(dropdownOptions[1].attr('value')).toBe('some-sentence-code')
-  })
-
-  it('renders the sentence date input', () => {
-    const cheerioPage = renderWithCheerio()
-
-    const dateInput = cheerioPage('#sentence-date')
-    expect(dateInput.length).toBe(1)
-
-    const dateHeading = cheerioPage('legend')
-    expect(dateHeading.text()).toContain('Sentence date')
-  })
 })
