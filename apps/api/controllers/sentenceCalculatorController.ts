25e77b3799d3f757d03f4f6cca3d0006647d1feb
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-09-02 15:47:27 +0100

    refactor the controller extract logic from router as ben suggested

--- apps/api/controllers/sentenceCalculatorController.ts
+++ apps/api/controllers/sentenceCalculatorController.ts
@@ -1,15 +1,26 @@
-import { InputSentences, AdjustmentTypes } from "../services/sentenceCalculator/types";
-import SentenceCalculator from "../services/sentenceCalculator/SentenceCalculator"
+import { InputSentences, OutputCalculation, AdjustmentTypes } from '../services/sentenceCalculator/types'
+import SentenceCalculator from '../services/sentenceCalculator/SentenceCalculator'
 
-export class SentenceCalculatorController{
-    constructor (sentence: InputSentences) {
+// TODO: extract this in parser class which also does simple validation use zod
+export default function sentenceCalculatorController(sentence: InputSentences): OutputCalculation {
+    
+    //TODO replace this temp deserislizing witg zod
+    sentence.inputIndividualSentences = sentence.inputIndividualSentences.map(term => ({
+        ...term,
+        from: new Date(term.from),
+    }))
 
-        // TODO: extract this in parser class which also does simple validation use zod 
-        const sentenceCalc = new SentenceCalculator(sentence)
-        if ((sentence.remandAdjustment?.days ?? 0) > 0) {
-            sentenceCalc.adjustCalculation(AdjustmentTypes.remand);
-        }
-        const finalCalObj = sentenceCalc.getCalculation();
-        return finalCalObj;
+    //conditionally deserialises
+  if (sentence.remandAdjustment) {
+    sentence.remandAdjustment = {
+      ...sentence.remandAdjustment,
+      startDate: new Date(sentence.remandAdjustment.startDate),
     }
+  }
+  
+  const sentenceCalc = new SentenceCalculator(sentence)
+  if ((sentence.remandAdjustment?.days ?? 0) > 0) {
+    sentenceCalc.adjustCalculation(AdjustmentTypes.remand)
+  }
+  return sentenceCalc.getCalculation()
 }
