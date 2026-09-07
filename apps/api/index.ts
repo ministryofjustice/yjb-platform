43678fa5058e7623a706d9d00e0e32e6823044bb
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-09-07 10:55:16 +0100

    test deployment pipeline

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,6 @@
 import createApp from './app'
 import applicationInfo from './applicationInfo'
 
+// add this comment to test deployment pipeline
 const app = createApp(applicationInfo())
 export default app
