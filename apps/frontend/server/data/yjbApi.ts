44cb82a5f07f2c2b429344caaa24250d1f9a6ef3
Jasper Jackson <jasper.jackson1@yjb.gov.uk>
2026-09-07 12:42:00 +0100

    Replace dummy data being sent to the endpoint with real payload

--- apps/frontend/server/data/yjbApi.ts
+++ apps/frontend/server/data/yjbApi.ts
@@ -13,47 +13,7 @@ export default class YjbApiClient extends RestClient {
   }
 
   async calculateDtoSentence(payload: InputSentences): Promise<OutputCalculation> {
-    const dummyPayload: InputSentences = {
-      offenderName: 'Test Offender',
-      remandAdjustment: {
-        name: 'remand',
-        startDate: new Date('2026-06-14'),
-        days: payload.remandAdjustment.days,
-      },
-      inputIndividualSentences: [
-        {
-          from: new Date('2026-06-29'),
-          durationMonths: 11,
-        },
-      ],
-    }
-
-    const result: Promise<OutputCalculation> = this.post({ path: '/calculations', data: dummyPayload })
-
-    return result
-  }
-
-  async realEndpointWithDummyData(): Promise<OutputCalculation> {
-    const dummyData: InputSentences = {
-      offenderName: 'Test Offender',
-      // inputAdjustments: {
-      //   remand: 15,
-      //   remandStartDate: new Date('2026-06-14'),
-      //   taggedBailDays: 0,
-      // },
-      inputIndividualSentences: [
-        {
-          from: new Date('2026-06-29'),
-          durationMonths: 11,
-        },
-      ],
-    }
-
-    // curl -X POST localhost:3001/calculations \
-    // -H "Content-Type: application/json" \
-    // -d '{"offenderName":"Test Offender","inputIndividualSentences":[{"from":"2026-06-29","durationMonths":11}]}'
-
-    const result: Promise<OutputCalculation> = this.post({ path: '/calculations', data: dummyData })
+    const result: Promise<OutputCalculation> = this.post({ path: '/calculations', data: payload })
 
     return result
   }
