cbe950f2dbf3f3abdc5ddaec5320af94ff76d2da
Jasper Jackson <jasper.jackson1@yjb.gov.uk>
2026-09-07 12:01:21 +0100

    Removed test comment

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,6 +1,5 @@
 import createApp from './app'
 import applicationInfo from './applicationInfo'
 
-// add this comment to test deployment pipeline
 const app = createApp(applicationInfo())
 export default app
