6ea6923d82942c96545d9af0df2b8425892ef6f8
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-09-11 11:38:32 +0100

    add missing tests

--- apps/api/services/sentenceCalculator/CalculationService.test.ts
+++ apps/api/services/sentenceCalculator/CalculationService.test.ts
@@ -1,7 +1,5 @@
-import { calculateDTOSentence } from './CalculationService'
+import calculateDTOSentence from './CalculationService'
 import { AdjustmentTypes, InputSentences, OutputCalculation } from './types'
-
-
 
 const remandInput: InputSentences = {
   offenderName: 'Test Offender',
@@ -52,52 +50,165 @@ const remandAndTaggedBailInput: InputSentences = {
 }
 
 describe('calculateDTOSentence', () => {
-    it('returns full calculation for 11 months sentence starting on 2026-06-29 no adjustments', ()=>{
-        const noRemandInput: InputSentences = {
-            offenderName: 'Test Offender',
-            remandAdjustment: {
-                name: AdjustmentTypes.remand,
-                days: 0,
-                startDate: new Date(),
-            },
-            taggedBailAdjustment: {
-                name: AdjustmentTypes.taggedBail,
-                days: 0,
-            },
-            inputIndividualSentences: [
-                {
-                from: new Date('2026-06-29'),
-                durationMonths: 11,
-                },
-            ],
-        }
+  it('returns full calculation for 11 months sentence starting on 2026-06-29 no adjustments', () => {
+    const noRemandInput: InputSentences = {
+      offenderName: 'Test Offender',
+      remandAdjustment: {
+        name: AdjustmentTypes.remand,
+        days: 0,
+        startDate: new Date(),
+      },
+      taggedBailAdjustment: {
+        name: AdjustmentTypes.taggedBail,
+        days: 0,
+      },
+      inputIndividualSentences: [
+        {
+          from: new Date('2026-06-29'),
+          durationMonths: 11,
+        },
+      ],
+    }
 
-        const expectedNoRemandOutput: OutputCalculation = {
-        calculatedTerms: [
-          {
-            inputSentence: { from: new Date('2026-06-29'), durationMonths: 11 },
-            totalDaysInTerm: 334,
-            totalDaysMTD: 167,
-            sled: new Date('2027-05-28'),
-            mtd: new Date('2026-12-12'),
-          },
-        ],
-        effectiveDates: {
-          totalNumberOfRemandAndTaggedBailDays: 0,
+    const expectedNoRemandOutput: OutputCalculation = {
+      calculatedTerms: [
+        {
+          inputSentence: { from: new Date('2026-06-29'), durationMonths: 11 },
+          totalDaysInTerm: 334,
+          totalDaysMTD: 167,
           sled: new Date('2027-05-28'),
           mtd: new Date('2026-12-12'),
-          TUSED: new Date(0),
         },
-        ltd: new Date('2027-01-12'),
-        etd: new Date('2026-11-12'),
-        effectiveDatesPastAdjustments: [],
-      }
+      ],
+      effectiveDates: {
+        totalNumberOfRemandAndTaggedBailDays: 0,
+        sled: new Date('2027-05-28'),
+        mtd: new Date('2026-12-12'),
+        TUSED: new Date(0),
+      },
+      ltd: new Date('2027-01-12'),
+      etd: new Date('2026-11-12'),
+      effectiveDatesPastAdjustments: [],
+    }
 
-        expect(calculateDTOSentence(noRemandInput)).toEqual(expectedNoRemandOutput)
-    })
+    expect(calculateDTOSentence(noRemandInput)).toEqual(expectedNoRemandOutput)
+  })
 
-    // it returns full calculation for 11 months sentence starting on 2026-06-29 15 days remand
-    // it returns full calculation for 11 months sentence starting on 2026-06-29 15 days Tagged Bail
-    // it returns full calculation for 11 months sentence starting on 2026-06-29 15 days remand 4 tagged bail
+  it('returns full calculation for 11 months sentence starting on 2026-06-29 with 15 days remand', () => {
+    const expectedRemandOutput: OutputCalculation = {
+      calculatedTerms: [
+        {
+          inputSentence: { from: new Date('2026-06-29'), durationMonths: 11 },
+          totalDaysInTerm: 334,
+          totalDaysMTD: 167,
+          sled: new Date('2027-05-28'),
+          mtd: new Date('2026-12-12'),
+        },
+      ],
+      effectiveDates: {
+        totalNumberOfRemandAndTaggedBailDays: 15,
+        sled: new Date('2027-05-13'),
+        mtd: new Date('2026-11-27'),
+        TUSED: new Date(0),
+      },
+      ltd: new Date('2026-12-27'),
+      etd: new Date('2026-10-27'),
+      effectiveDatesPastAdjustments: [
+        {
+          adjustmentReason: 'remand',
+          adjustmentParameters: remandInput.remandAdjustment!,
+          pastEffectiveDates: {
+            totalNumberOfRemandAndTaggedBailDays: 0,
+            sled: new Date('2027-05-28'),
+            mtd: new Date('2026-12-12'),
+            TUSED: new Date(0),
+          },
+        },
+      ],
+    }
+
+    expect(calculateDTOSentence(remandInput)).toEqual(expectedRemandOutput)
+  })
+
+  it('returns full calculation for 11 months sentence starting on 2026-06-29 with 15 days tagged bail', () => {
+    const expectedTaggedBailOutput: OutputCalculation = {
+      calculatedTerms: [
+        {
+          inputSentence: { from: new Date('2026-06-29'), durationMonths: 11 },
+          totalDaysInTerm: 334,
+          totalDaysMTD: 167,
+          sled: new Date('2027-05-28'),
+          mtd: new Date('2026-12-12'),
+        },
+      ],
+      effectiveDates: {
+        totalNumberOfRemandAndTaggedBailDays: 15,
+        sled: new Date('2027-05-13'),
+        mtd: new Date('2026-11-27'),
+        TUSED: new Date(0),
+      },
+      ltd: new Date('2026-12-27'),
+      etd: new Date('2026-10-27'),
+      effectiveDatesPastAdjustments: [
+        {
+          adjustmentReason: 'taggedBail',
+          adjustmentParameters: remandTaggedBailInput.taggedBailAdjustment!,
+          pastEffectiveDates: {
+            totalNumberOfRemandAndTaggedBailDays: 0,
+            sled: new Date('2027-05-28'),
+            mtd: new Date('2026-12-12'),
+            TUSED: new Date(0),
+          },
+        },
+      ],
+    }
+
+    expect(calculateDTOSentence(remandTaggedBailInput)).toEqual(expectedTaggedBailOutput)
+  })
+
+  it('returns full calculation for 11 months sentence starting on 2026-06-29 with 15 days remand and 4 days tagged bail', () => {
+    const expectedRemandAndTaggedBailOutput: OutputCalculation = {
+      calculatedTerms: [
+        {
+          inputSentence: { from: new Date('2026-06-29'), durationMonths: 11 },
+          totalDaysInTerm: 334,
+          totalDaysMTD: 167,
+          sled: new Date('2027-05-28'),
+          mtd: new Date('2026-12-12'),
+        },
+      ],
+      effectiveDates: {
+        totalNumberOfRemandAndTaggedBailDays: 19,
+        sled: new Date('2027-05-09'),
+        mtd: new Date('2026-11-23'),
+        TUSED: new Date(0),
+      },
+      ltd: new Date('2026-12-23'),
+      etd: new Date('2026-10-23'),
+      effectiveDatesPastAdjustments: [
+        {
+          adjustmentReason: 'remand',
+          adjustmentParameters: remandAndTaggedBailInput.remandAdjustment!,
+          pastEffectiveDates: {
+            totalNumberOfRemandAndTaggedBailDays: 0,
+            sled: new Date('2027-05-28'),
+            mtd: new Date('2026-12-12'),
+            TUSED: new Date(0),
+          },
+        },
+        {
+          adjustmentReason: 'taggedBail',
+          adjustmentParameters: remandAndTaggedBailInput.taggedBailAdjustment!,
+          pastEffectiveDates: {
+            totalNumberOfRemandAndTaggedBailDays: 15,
+            sled: new Date('2027-05-13'),
+            mtd: new Date('2026-11-27'),
+            TUSED: new Date(0),
+          },
+        },
+      ],
+    }
+
+    expect(calculateDTOSentence(remandAndTaggedBailInput)).toEqual(expectedRemandAndTaggedBailOutput)
+  })
 })
-
