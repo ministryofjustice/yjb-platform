bc671d30e1aacf8f240fcbeedccd1ca880121c56
Jasper Jackson <jasper.jackson1@yjb.gov.uk>
2026-09-11 12:55:27 +0100

    Add separate package for shared types Update dockerfiles to copy shared types

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
+  moduleNameMapper: {
+    '^@yjb-platform/shared-types$': '<rootDir>/../../packages/shared-types/index.ts',
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['json', 'text', 'lcov', 'clover'],
