a524d5d857722bd0bb54f8c5a0643c444921d3cd
Kate Staneva <kate.staneva@yjb.gov.uk>
2026-09-18 14:56:05 +0100

    make lint happy

--- apps/api/controllers/sentenceCalculatorMapper.ts
+++ apps/api/controllers/sentenceCalculatorMapper.ts
@@ -4,7 +4,7 @@ import { calculateAdjustmentStart } from '../services/sentenceCalculator/lib'
 export function parseInputSentences(body: unknown): InputSentences {
   const parsedInput = inputSentencesSchema.parse(body)
   // for remand with no start date extract the start date
-  if (parsedInput.remandAdjustment && !(parsedInput.remandAdjustment!.startDate)) {
+  if (parsedInput.remandAdjustment && !parsedInput.remandAdjustment!.startDate) {
     parsedInput.remandAdjustment!.startDate = calculateAdjustmentStart(
       parsedInput.inputIndividualSentences[0].from,
       parsedInput.remandAdjustment!.days,
